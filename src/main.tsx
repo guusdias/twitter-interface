d3fa308f28987d6ca907292c5e37a438ac2995ae
gustapine <gustavohd34@gmail.com>
2024-01-11 16:33:54 -0300

    feat: forms tweet

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./global.css";
 import TwitterLogo from "./assets/twitter-logo.svg";
-import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, User } from "phosphor-react";
+import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, Sparkle, User } from "phosphor-react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -37,7 +37,19 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         </nav>
         <button className="new-tweet">Tweet</button>
       </aside>
-      <div className="content"></div>
+      <div className="content">
+        <main className="timeline">
+          <div className="timeline-header">Home
+          <Sparkle/></div>
+          <form action="" className="new-tweet-form">
+            <label htmlFor="tweet">
+              <img src="https://github.com/guusdias.png" alt="Gustavo Dias" />
+              <textarea id="tweet" placeholder="What's happening?"></textarea>
+            </label>
+            <button type="submit">Tweet</button>
+          </form>
+        </main>
+      </div>
     </div>
   </React.StrictMode>
 );
