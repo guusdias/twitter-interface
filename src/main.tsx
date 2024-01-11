b5601a2242d942fd482d82d2708b89e0327301c7
gustapine <gustavohd34@gmail.com>
2024-01-11 16:08:41 -0300

    aside menu bar

---
+++ src/main.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
+import TwitterLogo from "./assets/twitter-logo.svg";
+import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, User } from "phosphor-react";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <div className="layout">
+      <aside className="sidebar">
+        <img className="logo" src={TwitterLogo} alt="Logo" />
+        <nav className="main-navigation">
+          <a className="active" href="">
+            <House weight="fill"/>
+            Home</a>
+          <a href="">
+            <Hash />
+            Explore</a>
+          <a href="">
+            <Bell/>
+            Notifications</a>
+          <a href="">
+            <Envelope/>
+            Messages</a>
+          <a href="">
+            <BookmarkSimple/>
+            Bookmarks</a>
+          <a href="">
+            <FileText/>
+            Lists</a>
+          <a href="">
+            <User/>
+            Profile</a>
+          <a href="">
+            <DotsThreeCircle/>
+            More</a>
+        </nav>
+        <button className="new-tweet">Tweet</button>
+      </aside>
+      <div className="content"></div>
+    </div>
+  </React.StrictMode>
+);
